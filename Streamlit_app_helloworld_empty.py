3f1c7ad036f72fb6d3da1970ef10b64d76e13c6b
polinalystopad25 <polina.lystopad@whu.edu>
2025-03-31 10:10:58 +0200

    Add files via upload

---
+++ Streamlit_app_helloworld_empty.py
@@ -0,0 +1,13 @@
+#import streamlit
+
+import streamlit as st
+
+# create a title Hellow World App
+
+st.title("Hello World App")
+# create a text input for the user to enter their name
+
+# create a button to say hello
+
+
+
